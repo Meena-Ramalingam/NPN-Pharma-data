769bebf009f4a61180ce6fb9da20afdd18608faf
Meena R <124681652+Meena-Ramalingam@users.noreply.github.com>
2024-08-20 22:16:59 +0530

    Chatbot page completed
    Chatbot page's features are added ,had to include the chatbot in it

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
