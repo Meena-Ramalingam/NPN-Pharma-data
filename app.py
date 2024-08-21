7b85e49dd8128f370ebeb63998a823f4c1fb168c
Meena R <124681652+Meena-Ramalingam@users.noreply.github.com>
2024-08-21 13:06:21 +0530

    Pages for mo1ab and mo1ae has been added

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ def index():
 def chatbot():
     return render_template('chatbot.html')
 
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+@app.route('/mo1ab')
+def mo1ab():
+    return render_template('mo1ab.html')
+@app.route('/mo1ae')
+def mo1ae():
+    return render_template('mo1ae.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
