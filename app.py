69c7de2d6e9098386ad1a373bcb4124c68d314c6
Meena R <124681652+Meena-Ramalingam@users.noreply.github.com>
2024-08-29 22:22:11 +0530

    working on integration

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import pickle
+import pmdarima
+import pandas as pd
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -9,6 +14,11 @@ def index():
 @app.route('/chatbot')
 def chatbot():
     return render_template('chatbot.html')
+
+
+@app.route('/prediction')
+def prediction():
+    return render_template('predictions.html')
 
 @app.route('/search')
 def search():
@@ -45,5 +55,38 @@ def details():
 @app.route('/drugs')
 def drugs():
     return render_template('drugs.html')
+
+
+drugs = ['M01AB', 'M01AE', 'N02BA', 'N02BE', 'N05B', 'N05C', 'R03', 'R06']
+models = {}
+for drug in drugs:
+    with open(f'models/auto_arima_model_{drug}.pkl', 'rb') as file:
+        models[drug] = pickle.load(file)
+
+@app.route('/predict_single', methods=['POST'])
+def predict_single():
+    single_date = request.form['single_date']
+    date_range = pd.date_range(start=single_date, end=single_date, freq='D')
+    
+    predictions = {}
+    for drug in drugs:
+        predictions[drug] = models[drug].predict(n_periods=1)
+    
+    return jsonify(predictions)
+
+@app.route('/predict_range', methods=['POST'])
+def predict_range():
+    start_date = request.form['start_date']
+    end_date = request.form['end_date']
+    date_range = pd.date_range(start=start_date, end=end_date, freq='D')
+    
+    predictions = {}
+    for drug in drugs:
+        predictions[drug] = models[drug].predict(n_periods=len(date_range)).sum()
+    
+    return jsonify(predictions)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True,port=5001)
