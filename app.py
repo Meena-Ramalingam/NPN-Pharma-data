edc4239d311ee98ebdc4306e9910b12854250d11
Meena R <124681652+Meena-Ramalingam@users.noreply.github.com>
2024-08-30 10:05:37 +0530

    Backend integration of the prediction module has been done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import pmdarima
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -56,7 +57,62 @@ def details():
 def drugs():
     return render_template('drugs.html')
 
+models_folder = 'models'
 
+# Load models for weekly and daily predictions
+drugs = ['M01AB', 'M01AE', 'N02BA', 'N02BE', 'N05B', 'N05C', 'R03', 'R06']
+
+models_weekly = {}
+models_daily = {}
+
+for drug in drugs:
+    with open(os.path.join(models_folder, f'auto_arima_model_Week_{drug}.pkl'), 'rb') as file:
+        models_weekly[drug] = pickle.load(file)
+    with open(os.path.join(models_folder, f'auto_arima_model_{drug}.pkl'), 'rb') as file:
+        models_daily[drug] = pickle.load(file)
+
+@app.route('/predict')
+def predict():
+    return render_template('predictions.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    date_type = request.form.get('dateType')
+
+    if date_type == 'daily':
+        single_date = request.form.get('singleDate')
+        if single_date:
+            result = predict_daily_sales(single_date)
+    elif date_type == 'weekly':
+        start_date = request.form.get('startDate')
+        end_date = request.form.get('endDate')
+        if start_date and end_date:
+            result = predict_weekly_sales(start_date, end_date)
+    
+    return jsonify(result)
+
+def predict_daily_sales(single_date):
+    start_date = '2019-11-30'  # Last date in the dataset
+    date_range = pd.date_range(start=start_date, end=single_date, freq='D')
+
+    predictions = {}
+    for drug in drugs:
+        predictions[drug] = models_daily[drug].predict(n_periods=len(date_range)) / 30
+
+    predictions_df = pd.DataFrame(predictions, index=date_range)
+    result = predictions_df.loc[single_date].to_dict()
+    return result
+
+def predict_weekly_sales(start_date, end_date):
+    date_range = pd.date_range(start=start_date, end=end_date, freq='7D')
+
+    predictions = {}
+    for drug in drugs:
+        predictions[drug] = models_weekly[drug].predict(n_periods=len(date_range))
+
+    predictions_df = pd.DataFrame(predictions, index=date_range)
+    result = predictions_df.loc[end_date].to_dict()
+    return result
 
 
 
